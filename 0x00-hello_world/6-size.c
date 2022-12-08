142a20fc38c4645ebe45acf67b85c9195de0d3c9
ALX2024 <ethiocgi@gmail.com>
2022-12-08 12:08:27 -0800

    adding files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of char: %i byte(s)\n", sizeo
+			f(char));
+	printf("Size of char: %i byte(s)\n", sizeo
+			f(char));
+	printf("Size of char: %i byte(s)\n", sizeo                                                    	f(char));
+
