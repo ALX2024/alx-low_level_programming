c0700389a2cd46e23b82914bf3789a7cfb9d19f7
ALX2024 <ethiocgi@gmail.com>
2022-12-10 02:57:31 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,12 @@ int main(void)
 {
 	printf("Size of char: %i byte(s)\n", sizeo
 			f(char));
-	printf("Size of char: %i byte(s)\n", sizeo
-			f(char));
-	printf("Size of char: %i byte(s)\n", sizeo                                                    	f(char));
+	printf("Size of an int: %i byte(s)\n", sizeo
+			f(int));
+	printf("Size of a long int: %i byte(s)\n", sizeo                                                    	f(long int));
+	printf("Size of a long long int: %i byte(s)\n",sizeo
+			f(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeo
+			f(float));
+	return (0);
 
